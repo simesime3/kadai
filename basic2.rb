16f8247c66956b1667ea6238cfc00df1813aad77
simesime3 <49155426+simesime3@users.noreply.github.com>
2019-04-10 22:51:01 +0900

    Add files via upload

---
+++ basic2.rb
@@ -0,0 +1,31 @@
+n = 3
+if n == 3
+  puts n
+end
+
+if n == 3 || n ==4
+  puts "nは３か４です。"
+end
+
+if n == 3
+ puts "nは3です。"
+elsif n == 4
+ puts "nは４です。"
+end
+
+for num in 1..5 do
+  puts "こんにちは"
+end
+
+[1, 2, 3, 4, 5].each do |num|
+  puts num
+end
+
+num = 6
+if num == 2
+  puts "2です"
+elsif num == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
