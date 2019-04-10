b5a4945ec5e95bd0989cbc903f271cf8a0c94e89
古賀克磨 <kogakatsuma@kogakatsumanoMacBook-Pro.local>
2019-04-10 22:43:48 +0900

    first

---
+++ basic.rb
@@ -0,0 +1,23 @@
+puts "初めてのruby"
+puts "Ruby" + "始めました"
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+num_array = [1, 2, 3, 4]
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+produce = {name: "古賀克磨", birthday: 19910803, blood: "A"}
+def plus_ruby(ruby)
+  print ruby + "ruby"
+end
+puts plus_ruby("like")
+puts plus_ruby("yhee")
+
+def plus_one(value)
+  print value + 1
+end
+puts plus_one(4)
+puts plus_one(10)
